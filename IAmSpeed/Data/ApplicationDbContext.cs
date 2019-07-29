0ab8fb69f36e03cbd715b813770667fdef2092ef
Alex Thacker <thacker.alex@yahoo.com>
2019-07-29 09:24:43 -0500

    first push

--- IAmSpeed/Data/ApplicationDbContext.cs
+++ IAmSpeed/Data/ApplicationDbContext.cs
@@ -11,6 +11,10 @@ namespace IAmSpeed.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Following> Followings { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Segment> Segments { get; set; }
+
 
     }
 }
--- IAmSpeed/Models/Segment.cs
+++ IAmSpeed/Models/Segment.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace IAmSpeed.Models
 {
-    public class Section
+    public class Segment
     {
         public int Id { get; set;  }
         public string Name { get; set; }
